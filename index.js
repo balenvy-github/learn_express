cd5010dc4d56c5642e2abe7d1968d5c68b602cee
iqbal wahyudi <iqbalgokil025@gmail.com>
2020-11-12 15:48:02 +0800

    full crud

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
 
 const app = express();
+app.use(bodyParser.json());
 const authRoutes = require("./src/routes/auth");
 const blogRoutes = require("./src/routes/blog");
+const { static } = require("express");
 
-app.use(bodyParser.json());
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(
+  multer({
+    storage: fileStorage,
+    fileFilter: fileFilter,
+  }).single("image")
+);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -30,4 +62,14 @@ app.use((error, req, res, next) => {
   });
 });
 
-app.listen(4000);
+mongoose
+  .connect(
+    "mongodb+srv://iqbal:5wyTo3RIgop6aDa7@cluster0.mvtos.mongodb.net/blog?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    app.listen(4000, () => console.log("connection success"));
+  })
+  .catch((err) => console.log(err));
+
+// mongo pass 9arKitwESf9d0BBw // GMbEjdQpxd4FOeSj // 5wyTo3RIgop6aDa7
--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,9 +1,9 @@
 const { validationResult } = require("express-validator");
+const BlogPost = require("../models/blog");
+const path = require("path");
+const fs = require("fs");
 
 exports.createBlogPost = (req, res, next) => {
-  const title = req.body.title;
-  const body = req.body.body;
-
   const error = validationResult(req);
 
   if (!error.isEmpty()) {
@@ -14,21 +14,160 @@ exports.createBlogPost = (req, res, next) => {
     throw err;
   }
 
-  const result = {
-    message: "Create Blog Post Success",
-    data: {
-      post_id: 1,
-      title: title,
-      image: "image.png",
-      body: body,
-      created_at: "12/10/2020",
-      author: {
-        uid: 1,
-        name: "testing",
-      },
+  if (!req.file) {
+    const err = new Error("Image Harus di Upload");
+    err.errorStatus = 422;
+
+    throw err;
+  }
+
+  const title = req.body.title;
+  const body = req.body.body;
+  const image = req.file.path;
+
+  const Posting = new BlogPost({
+    title: title,
+    body: body,
+    image: image,
+    author: {
+      uid: 1,
+      name: "Author Name",
     },
-  };
-  res.status(201).json({
-    result,
   });
+
+  Posting.save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Create Blog Post Success",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      console.log("error: ", err);
+    });
+};
+
+exports.getAllBlogPost = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = req.query.perPage || 5;
+  let totalItems;
+
+  BlogPost.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return BlogPost.find()
+        .skip((parseInt(currentPage) - 1) * parseInt(perPage))
+        .limit(parseInt(perPage));
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Data berhasil dipanggil",
+        data: result,
+        total_data: totalItems,
+        per_page: parseInt(perPage),
+        current_page: parseInt(currentPage),
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getBlogPostById = (req, res, next) => {
+  const postId = req.params.postId;
+  BlogPost.findById(postId)
+    .then((result) => {
+      if (!result) {
+        const error = new Error("Blog Post Tidak Ditemukan");
+        error.errorStatus = 404;
+        throw error;
+      }
+      res.status(200).json({
+        message: "Data blog post berhasil dipanggil",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.updatePostById = (req, res, next) => {
+  const error = validationResult(req);
+
+  if (!error.isEmpty()) {
+    const err = new Error("Input Value Tidak Sesuai");
+    err.errorStatus = 400;
+    err.data = error.array();
+
+    throw err;
+  }
+
+  if (!req.file) {
+    const err = new Error("Image Harus di Upload");
+    err.errorStatus = 422;
+
+    throw err;
+  }
+
+  const title = req.body.title;
+  const body = req.body.body;
+  const image = req.file.path;
+  const postId = req.params.postId;
+
+  BlogPost.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const err = new Error("Blog Post Tidak Ditemukan");
+        err.errorStatus = 404;
+        throw err;
+      }
+
+      post.title = title;
+      post.body = body;
+      post.image = image;
+
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Update Berhasil",
+        data: result,
+      });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
+
+exports.deleteBlogPost = (req, res, next) => {
+  const postId = req.params.postId;
+  BlogPost.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Blog Post Tidak Ditemukan");
+        error.errorStatus = 404;
+        throw error;
+      }
+      removeImage(post.image);
+      return BlogPost.findByIdAndRemove(postId);
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Hapus blog post berhasil",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+const removeImage = (filePath) => {
+  console.log("file path:", filePath);
+  console.log("dirname:", __dirname);
+
+  filePath = path.join(__dirname, "../..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
 };
